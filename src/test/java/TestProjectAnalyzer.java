c84fe6a5299edb01368c2ccfa9ea138cf3c4839c
natebass <nw3455@gmail.com>
2024-10-17 13:05:56 -0700

    chore: Update VS Code extensions and Maven dependencies

--- src/test/java/TestProjectAnalyzer.java
+++ src/test/java/TestProjectAnalyzer.java
@@ -4,8 +4,8 @@ public class TestProjectAnalyzer {
     @Test
     public void testJavaProject() {
         // Test the command line interface
-//        String[] args = {"/home/nwb/Source/Repos/be-local-graph-java-04", "java"};
-//        ProjectAnalyzer.AnalyzeWorkingDirectory(args);
-        String[] args = {"/home/nwb/Source/Repos/be-local-graph-java-04/src/main/resources/doap.n3", "java"};
+        // String[] args = { "/home/nwb/Source/Repos/be-local-graph-java-04", "java" };
+        // ProjectAnalyzer.AnalyzeWorkingDirectory(args);
+        String[] args = { "/home/nwb/Source/Repos/be-local-graph-java-04/src/main/resources/doap.n3", "java" };
     }
 }
